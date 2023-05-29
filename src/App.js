514afaf640f530b98137ab1dab522cbf4a8a2986
beastvaibhv <92950515+beastvaibhv@users.noreply.github.com>
2023-05-29 14:36:49 +0530

    Revert "Youtube Clone"
    This reverts commit 959ac0daa5eb6a127c53ae22f901d7e01835d52e.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import Header from './components/Header';
-import Body from './components/Body';
-import Error from './components/Error';
-import WatchPage from './components/WatchPage';
+import logo from './logo.svg';
 import './App.css';
-import { Provider } from 'react-redux';
-import store from './utils/store';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import MainContainer from './components/MainContainer';
-
-const AppRouter = createBrowserRouter([
-  {path:"/",
-   element: <Body/>,
-   errorElement : <Error/>,
-  children:[
-    {path:"/",
-     element: <MainContainer/>},
-    {path:"watch",
-     element: <WatchPage/>},
-  ]}
-]);
 
 function App() {
   return (
-    <Provider store={store}>
-    <div className="">
-      
-    <Header/>
-   
-    <RouterProvider router= {AppRouter}/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    
-    </Provider>
   );
 }
 
